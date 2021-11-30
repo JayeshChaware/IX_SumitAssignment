a460ecc34e2ba811a1b49d2c82a6b08d01d7e768
acer <acer@IN-JAYESH>
2021-11-30 12:54:14 +0530

    Add project files.

---
+++ ClassLibCode/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassLibCode
+{
+    public static class Class1
+    {
+        public static int TwoNumSum(int numOne, int numTwo)
+        {
+            return numOne + numTwo;
+        }
+        public static int TwoNumProduct(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+        public static int TwoNumSubtraction(int numOne, int numTwo)
+        {
+            return numOne - numTwo;
+        }
+        public static int TwoNumDivision(int numOne, int numTwo)
+        {
+            return numOne / numTwo;
+        }
+        public static int NumMod(int numOne, int numTwo)
+        {
+            return numOne % numTwo;
+        }
+    }
+}
---
+++ ClassLibExe/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ClassLibCode;
+namespace ClassLibExe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Class1.TwoNumSum(10, 12));
+            
+        }
+    }
+}
