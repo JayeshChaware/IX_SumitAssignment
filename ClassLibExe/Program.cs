f46d87e3b7fe8d5a5359dd3be308de98694ef540
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-02 18:16:57 +0530

    monday assignment

--- ClassLibExe/Program.cs
+++ ClassLibExe/Program.cs
@@ -7,7 +7,11 @@ namespace ClassLibExe
         static void Main(string[] args)
         {
             Console.WriteLine(Class1.TwoNumSum(10, 12));
-            
+            Console.WriteLine(Class1.TwoNumProduct(10, 12));
+            Console.WriteLine(Class1.TwoNumSubtraction(12, 10));
+            Console.WriteLine(Class1.TwoNumDivision(12, 10));
+
+
         }
     }
 }
