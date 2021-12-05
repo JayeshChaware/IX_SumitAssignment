47d908c4e5209bdb6399226bfac1036495f9ce16
JayeshChaware <jayesh.chaware-imed@bvp.edu.in>
2021-12-06 00:38:21 +0530

    up to static member

--- dec_3_assignment/Student.cs
+++ dec_3_assignment/Student.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dec_3_assignment
 {
@@ -7,9 +8,12 @@ namespace dec_3_assignment
         public string Name { get; set; }
         public int Age { get; set; }
         public Gender Sex { get; set; }
-        static string course;
+
+        public static string Course => course;
+
+        static readonly string course;
         public Student() { }//Default Constructor
-        public Student(int age, string name, Gender sex)// parameterized constructor
+        public Student(int age, string name, Gender sex)// parameterized constructor + call by value
         {
             if (name is null)
             {
@@ -23,7 +27,7 @@ namespace dec_3_assignment
 
             }
         }
-        public Student(Student Stud)//Copy Constructor
+        public Student(Student Stud)//Copy Constructor + call by reference
         {
             Name = Stud.Name;
             Age = Stud.Age;
@@ -41,11 +45,20 @@ class Counter
 {
     private Counter() { }
 
-    public static int currentCount;
+    public static int currentCount; //static member
 
     public static int IncrementCount()
     {
         return ++currentCount;
     }
-
+    object CounterObject = currentCount;//boxing
+    public void TotalSTudents()
+        {
+        Console.WriteLine("total students are :{0}", CounterObject);
+        }
+    public void ExapleOfUnboxing()
+    {
+        int objToInt = (int)CounterObject;//unboxing
+        Console.WriteLine("object to int conversion demo:{0}", objToInt);
+    }
 }
